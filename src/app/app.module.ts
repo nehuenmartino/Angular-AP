77fc87a43f104e65d29b1870b7c659e3533ffb6d
nehuenmartino <nehuenmartino@gmail.com>
2022-05-02 01:09:33 -0300

    Componentes divididos y funcionales. Previo a Servicios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { SocialMediaComponent } from './components/social-media/social-media.component';
+import { EmailComponent } from './components/email/email.component';
+import { AboutMeComponent } from './components/about-me/about-me.component';
+import { ExpComponent } from './components/exp/exp.component';
+import { EducationComponent } from './components/education/education.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    SocialMediaComponent,
+    EmailComponent,
+    AboutMeComponent,
+    ExpComponent,
+    EducationComponent,
+    SkillsComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
